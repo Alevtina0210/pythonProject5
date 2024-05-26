11e1e7b5659b6159804fd023249fe314df152af4
Alevtina0210 <alevtina.dolbilova@mail.ru>
2024-05-26 22:04:53 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars:
+    print("Я езжу на автомобиле марки ", [i])
+
+cars_count = 0
+for i in range(10):
+    print(cars_count)
+    cars_count += 10
+    print(cars_count)
